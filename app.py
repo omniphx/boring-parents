8500aba89f506068e2a094d7237f7a6089c1f1d2
Matthew Mitchener <mattjmitchener@gmail.com>
2017-02-12 12:47:54 -0500

    Deploying again with port set

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def home():
     return render_template('index.html', sentencesCount=3, boringStuff=boringStuff)
 
 if __name__ == "__main__":
-    port = int(5000)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
